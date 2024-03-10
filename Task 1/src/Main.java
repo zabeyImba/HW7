f5d11c9d94bc5effdaffed2be471cea402f2e704
zabey <melchakoff.sergey@yandex.ru>
2024-03-10 18:57:37 +0300

    the task is completed

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        boolean isCanCook = false;
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Введите кол-во Жабьих глаз: ");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите кол-во Слез вурдалака: ");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите кол-во Костей ворона: ");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите кол-во Пельменей: ");
+        dumplingsCount = new Scanner(System.in).nextInt();
+
+        if (toadEyesCount>=3&&ravenBonesCount>=1){
+            System.out.println("Вы можете создать Эликсир зоркости");
+            isCanCook = true;
+        }
+        if (ravenBonesCount>=2&&dumplingsCount>=4){
+            System.out.println("Вы можете создать Эликсир стойкости");
+            isCanCook = true;
+        }
+        if (ghoulTearsCount>=7&&dumplingsCount>=1&&toadEyesCount>=2){
+            System.out.println("Вы можете создать Эликсир скрытности");
+            isCanCook = true;
+        }
+        if (ghoulTearsCount>=5&&dumplingsCount>=10&&toadEyesCount>=4&&ravenBonesCount>=3){
+            System.out.println("Вы можете создать Запретный эликсир");
+            isCanCook = true;
+        }
+        if(!isCanCook){
+            System.out.println("Вы не сможете приготовить ни один Эликсир!");
+        }
+
+//        while (isCanCook){
+//            System.out.println("Вы можете приготовить");
+//        }
+
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
