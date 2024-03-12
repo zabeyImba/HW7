a8866b8c2dd9914a73fa20da696240064686d8ab
zabey <melchakoff.sergey@yandex.ru>
2024-03-12 17:07:23 +0300

    The task was completed! The task was completed in two way!

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+//            for (int id = 1; id <=100 ; id++) {
+//                if (id > 1 && id <= 100) {
+//                    System.out.println("Уволен айтишник с id: " + id);
+//                }
+//            }
+//
+//        }
         //todo Тут увольняем айтишников
         int id = 100;
-        while (id <= 100&&id>1) {
-                System.out.println("Уволен айтишник с id: " + id);
-                id = id - 2;
+        while (id <= 100 && id > 1) {
+            System.out.println("Уволен айтишник с id: " + id);
+            id = id - 2;
         }
     }
 }
