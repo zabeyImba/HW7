d0846802110e01f1bd9c1b858691a1954608ad8c
zabey <melchakoff.sergey@yandex.ru>
2024-03-12 16:53:12 +0300

    The task was completed

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int id = 100;
+        while (id <= 100&&id>1) {
+                System.out.println("Уволен айтишник с id: " + id);
+                id = id - 2;
+        }
     }
 }
